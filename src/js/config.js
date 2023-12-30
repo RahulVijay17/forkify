09cd2526bde8a2c91b4c4c7bcf50c53d3a71e54d
RahulVijay17 <frederickraghul@gmail.com>
2023-12-31 02:06:54 +0530

    forkify app using js

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '32307cdb-7868-4be2-9285-4e9f17f9d124';
+export const MODAL_CLOSE_SEC = 2.5;
